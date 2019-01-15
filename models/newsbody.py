f138e84ea8542cf0859ec1c73e94609b50e3a830
wisplu <dustin_chu@hotmail.com>
2019-01-15 22:38:56 +0800

    title body inser ok

--- models/newsbody.py
+++ models/newsbody.py
@@ -6,24 +6,24 @@ class NewsBodyModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    pageUrl = db.Column(db.String(300))
+    pageurl = db.Column(db.String(300))
     body = db.Column(db.String(200))
-    playUrl = db.Column(db.String(50))
+    playurl = db.Column(db.String(50))
 
 
 
-    def __init__(self, date, pageUrl, body, playUrl):
+    def __init__(self, date, pageurl, body, playurl):
         self.date = date
-        self.pageUrl = pageUrl
+        self.pageurl = pageurl
         self.body = body
-        self.playUrl = playUrl
+        self.playurl = playurl
 
     def json(self):
-        return {'pageUrl': self.pageUrl, 'body': self.body, 'playUrl': self.playUrl}
+        return {'pageurl': self.pageurl, 'body': self.body, 'playurl': self.playurl}
 
     @classmethod
-    def find_by_name(cls, pageUrl):
-        return cls.query.filter_by(pageUrl=pageUrl).first()#return ItemModel object!
+    def find_by_name(cls, pageurl):
+        return cls.query.filter_by(pageurl=pageurl).first()#return ItemModel object!
 
 
     def save_to_db(self):
