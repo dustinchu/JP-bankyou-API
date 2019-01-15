0cc7a72fa545674edb4e819299520c7fbf4ea8ae
wisplu <dustin_chu@hotmail.com>
2019-01-15 22:52:43 +0800

    title body inser ok

--- resources/news.py
+++ resources/news.py
@@ -76,9 +76,9 @@ class News(Resource):
             driver.close()
             #將資料寫入資料庫 url ＝./XXX 沒分割過的  到時候內容用標題查得到url去串
             newsBody = NewsBodyModel(date=datetime.date.today(),
-                      pageUrl=pageUrl,
+                      pageurl=pageUrl,
                       body=bodyStr,
-                      playUrl="https://nhks-vh.akamaihd.net/i/news/easy/" + rulId[0] + ".mp4/master.m3u8")
+                      playurl="https://nhks-vh.akamaihd.net/i/news/easy/" + rulId[0] + ".mp4/master.m3u8")
             try:
                 newsBody.save_to_db()
                 # 順便查內容
