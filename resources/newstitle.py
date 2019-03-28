7269e447fe3634e61266ee8e1149157643b83bd9
wisplu <dustin_chu@hotmail.com>
2019-03-28 20:01:39 +0800

    newstitle return maxdate

--- resources/newstitle.py
+++ resources/newstitle.py
@@ -4,10 +4,11 @@ from models.newstitle import NewsTtitleModel
 
 
 #
-class NewsTitleList(Resource):
-    def get(self):
-        return list(map(lambda x: x.json(), NewsTtitleModel.query.all()))
 # class NewsTitleList(Resource):
 #     def get(self):
-#         return list(map(lambda x: x.json(), NewsTtitleModel.query(NewsTtitleModel, max(NewsTtitleModel.date).over(partition_by=NewsTtitleModel.identifier, order_by=NewsTtitleModel.value))))
-# #
+#         return list(map(lambda x: x.json(), NewsTtitleModel.query.all()))
+class NewsTitleList(Resource):
+    def get(self):
+        return list(map(lambda x: x.json(), NewsTtitleModel.query.from_statement('select * from newstitle where date=(select max(date) from newstitle)')))
+# rows = session.query(User).from_statement('SELECT * FROM user WHERE name=:name').params(name='user1')
+#首先，query() 可以使用 from_statement() 方法，直接以完整的 SQL 指令進行查詢，更可以搭配 params() 將變數代入 SQL 指令中。
