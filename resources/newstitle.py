0455c4a2ea17bd07b9fe484356dbe43c9b7143b1
wisplu <dustin_chu@hotmail.com>
2019-03-28 19:22:53 +0800

    add exercise

--- resources/newstitle.py
+++ resources/newstitle.py
@@ -4,10 +4,10 @@ from models.newstitle import NewsTtitleModel
 
 
 #
-# class NewsTitleList(Resource):
-#     def get(self):
-#         return list(map(lambda x: x.json(), NewsTtitleModel.query.all()))
 class NewsTitleList(Resource):
     def get(self):
-        return list(map(lambda x: x.json(), NewsTtitleModel.query(NewsTtitleModel, max(NewsTtitleModel.date).over(partition_by=NewsTtitleModel.identifier, order_by=NewsTtitleModel.value))))
-#
+        return list(map(lambda x: x.json(), NewsTtitleModel.query.all()))
+# class NewsTitleList(Resource):
+#     def get(self):
+#         return list(map(lambda x: x.json(), NewsTtitleModel.query(NewsTtitleModel, max(NewsTtitleModel.date).over(partition_by=NewsTtitleModel.identifier, order_by=NewsTtitleModel.value))))
+# #
