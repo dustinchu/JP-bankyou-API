635d9a5cf5133be6ba985020f3ac92c2640beb73
wisplu <dustin_chu@hotmail.com>
2019-03-28 19:18:33 +0800

    edit news date

--- resources/newstitle.py
+++ resources/newstitle.py
@@ -3,7 +3,11 @@ from models.newstitle import NewsTtitleModel
 
 
 
-
+#
+# class NewsTitleList(Resource):
+#     def get(self):
+#         return list(map(lambda x: x.json(), NewsTtitleModel.query.all()))
 class NewsTitleList(Resource):
     def get(self):
-        return list(map(lambda x: x.json(), NewsTtitleModel.query.all()))
+        return list(map(lambda x: x.json(), NewsTtitleModel.query(NewsTtitleModel, max(NewsTtitleModel.date).over(partition_by=NewsTtitleModel.identifier, order_by=NewsTtitleModel.value))))
+#
